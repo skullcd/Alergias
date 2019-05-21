cdf16455eb3ee2c6a3ba7db9a25ab5a8a8c8674b
Unknown <carlosd_eg@yahoo.com.mx>
2019-05-21 02:59:24 -0500

    Ultima version

--- js/funciones.js
+++ js/funciones.js
@@ -7,7 +7,31 @@ function ContenedorDefinicion() {
     }
 }
 
+var AreaDolor = {Cabeza:["Nariz", "Ojos", "Orejas", "Boca", "Garganta"], Torso:["Pecho", "Espalda", "Adomen"],
+ Brazo:["Mano", "Hombro", "Codo"],Pierna:["Rodilla", "Pies"]};
+
 function change_photo(tipo){
-    console.log(tipo);
+    console.log(AreaDolor[tipo]);
+    data="";
+    for(datos in AreaDolor[tipo]){
+      data +="<div style='height: 100px' class='swiper-slide tarjetas-opciones'>"+AreaDolor[tipo][datos]+"</div>";
+    }
+    document.getElementById("dolorEspecifico").innerHTML = data;
     document.getElementById("img_principal").setAttribute("src","imagenes/"+tipo+".jpg");
 }
+
+
+
+// function getData(){
+//   var request = new XMLHttpRequest();
+//
+//   request.open('GET', "sources/alergias.json");
+//   request.responseType = 'json';
+//   request.send();
+//
+//   console.log(request);
+//   request.onload = function() {
+//   var datos = request.response;
+//   console.log(datos);
+//   }
+// }
--- js/slider.js
+++ js/slider.js
@@ -9,19 +9,31 @@ function showSliderValue() {
   var dom = document.getElementById('cont_derecho');
     console.log(bulletPosition);
   if (bulletPosition >= 0 && bulletPosition < (0.4)) {
-      colorOne = "#42DB00";
-      colorTwo = "#0CE829";
+        // if (bulletPosition == 0) {colorOne = "#41BA2F";}
+        // if (bulletPosition == 0.1) {colorOne = "#2EAE1B";}
+        // if (bulletPosition == 0.2) {colorOne = "#0CA300";}
+        // if (bulletPosition == 0.3) {colorOne = "#078500";}
+        if (bulletPosition == 0) {colorOne = "#078500";}
+        if (bulletPosition == 0.1) {colorOne = "#0CA300";}
+        if (bulletPosition == 0.2) {colorOne = "#2EAE1B";}
+        if (bulletPosition == 0.3) {colorOne = "#41BA2F";}
+        colorTwo = "#096500";
         orientation = "180deg";
         console.log("iuno");
         rangeBullet.innerHTML = "<i class='fa fa-smile-o' aria-hidden='true'></i>"
   } else if(bulletPosition >= 0.4 && bulletPosition < (0.7)){
-        colorOne = "#E8B02C";
-        colorTwo = "#F2CB13";
+        if (bulletPosition == 0.4) {colorOne = "#FFD700";}
+        if (bulletPosition == 0.5) {colorOne = "#FFEB00";}
+        if (bulletPosition == 0.6) {colorOne = "#FFAD34";}
+        colorTwo = "#FD6F01";
         orientation = "180deg";
         rangeBullet.innerHTML = "<i class='fa fa-meh-o' aria-hidden='true'></i>"
   }else if(bulletPosition >= 0.7 && bulletPosition <= (1)){
-        colorOne = "#DB302A";
-        colorTwo = "#F26327";
+        if (bulletPosition == 1) {colorOne = "#8A0808";}
+        if (bulletPosition == 0.9) {colorOne = "#AD0505";}
+        if (bulletPosition == 0.8) {colorOne = "#CB0A0D";}
+        if (bulletPosition == 0.7) {colorOne = "#F50B0C";}
+        colorTwo = "#5F0304";
         orientation = "180deg";
        rangeBullet.innerHTML = "<i class='fa fa-frown-o' aria-hidden='true'></i>"
     }
