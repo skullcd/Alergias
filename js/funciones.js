578e3b68a9f58ee0a8ebd2f2653571bd13655736
Unknown <carlosd_eg@yahoo.com.mx>
2019-05-24 23:42:49 -0500

    Que cosas :v

--- js/funciones.js
+++ js/funciones.js
@@ -14,24 +14,63 @@ function change_photo(tipo){
     console.log(AreaDolor[tipo]);
     data="";
     for(datos in AreaDolor[tipo]){
-      data +="<div style='height: 100px' class='swiper-slide tarjetas-opciones'>"+AreaDolor[tipo][datos]+"</div>";
+      data +="<div style='height: 100px' onclick=\"getQuestions('"+AreaDolor[tipo][datos]+"', '"+tipo+"')\" class='swiper-slide tarjetas-opciones'>"+AreaDolor[tipo][datos]+"</div>";
     }
     document.getElementById("dolorEspecifico").innerHTML = data;
     document.getElementById("img_principal").setAttribute("src","imagenes/"+tipo+".jpg");
 }
 
+function getQuestions(tipo, area){
+  getData(area,tipo);
+}
 
+function getData(area, areaEspecifica){
+  var xmlhttp = new XMLHttpRequest();
+  var url = "sources/catalogoPreguntas.json";
 
-// function getData(){
-//   var request = new XMLHttpRequest();
-//
-//   request.open('GET', "sources/alergias.json");
-//   request.responseType = 'json';
-//   request.send();
-//
-//   console.log(request);
-//   request.onload = function() {
-//   var datos = request.response;
-//   console.log(datos);
-//   }
-// }
+  xmlhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    var myArr = JSON.parse(this.responseText);
+    console.log(areaEspecifica);
+    areaEspecifica = myArr.Preguntas.Cuerpo[area][areaEspecifica];
+    data="";
+    try {
+      for (var i = 0; i < Object.keys(areaEspecifica).length; i++) {
+        data+="<div class='swiper-slide'><div id="+Object.keys(areaEspecifica)[i]+" class='preguntas'><h1>"+areaEspecifica[Object.keys(areaEspecifica)[i]]+"</h1></div></div>";
+      }
+    } catch (e) {
+      data+="<div class='swiper-slide'><div class='preguntas'><h1>No hay preguntas</h1></div></div>";
+    }
+    document.getElementById("preguntas").innerHTML = data;
+    var swiper = new Swiper('.swiper-container3', {
+       slidesPerView: 1,
+       spaceBetween: 30,
+       loop: true,
+       pagination: {
+         el: '.swiper-pagination3',
+         clickable: true,
+       },
+       navigation: {
+         nextEl: '.swiper-button-next',
+         prevEl: '.swiper-button-prev',
+       },
+     });
+    }
+  };
+  xmlhttp.open("GET", url, true);
+  xmlhttp.send();
+}
+
+var datosArr=[];
+for (var i = 0; i < 53; i++) {
+  datosArr[i] = 0;
+}
+function getQuestion(){
+  // console.log(datosArr);
+  datosArr[document.getElementsByClassName("swiper-slide-active")[0].getElementsByTagName('div')[0].id] = parseInt(document.getElementById('rs-range-line').value);
+  console.log(datosArr);
+  document.getElementById('rs-range-line').value = 0;
+  showSliderValue();
+  // console.log(document.getElementsByClassName("swiper-slide-active")[0].getElementsByTagName('div')[0].id);
+  // console.log(document.getElementById('rs-range-line').value);
+}
