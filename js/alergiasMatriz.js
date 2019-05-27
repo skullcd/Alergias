02d175b06f8a5a5b42c9944f099fb5264225d662
Jonathan Palomeque <john.reb97@gmail.com>
2019-05-27 17:26:29 -0500

    Creacion de matriz Alergias
    Se genero la matriz de [9][53] con os correspondientes valoes de cada posicion.
    
    Se corrigieron errores de alergias.json

--- js/alergiasMatriz.js
+++ js/alergiasMatriz.js
@@ -1,48 +1,94 @@
 function generaMatriz(){
-    var request = new XMLHttpRequest();
-    request.open('GET', './sources/alergias.json');
-    request.responseType = 'json';
-    request.send();
 
-    request.onload = function(){
-        var data = request.response;
-        console.log(data);
-        console.log(data['Alergias']['Alergia a las mascotas']['Sintomas']['Boca']['Picazón en la garganta']['Calificacion']);
-        var body = data['Alergias']['Alergia a las mascotas']['Sintomas'];
-        console.log(JSON.stringify(body.Boca));
-    };
-    //console.log(alergias.response);
+    var matrizAlergias = [
+        [0], //hata llegar a 54 (total de sintomas).
+        [1],  //hata llegar a 54 (total de sintomas).
+        [2],
+        [3],
+        [4],
+        [5],
+        [6],
+        [7],
+        [8],
+        [9]
+    ];
 
-var matrizAlergias = [];
-var sintoma = 0;    
-var calificacio = 0;
-
-for (let i = 0; i < 10; i++) {
-    matrizAlergias[i] = [];
-    for (let x = 0; x < 53; x++) {
-        calificacio = data;
-        matrizAlergias[i][x] = 0;
+    //Rellena la matriz en las 53 posiciones con 0
+    for (let i = 0; i < matrizAlergias.length; i++) {
+        for (let x = 0; x < 54; x++) {
+            matrizAlergias[i][x] =  0;
+        }
     }
-    
-}
+
+    //pedir el archivo alergias.json
+    var request = new XMLHttpRequest();
+    var partesCuerpo;
+
+    request.onreadystatechange = function(){
+        if (this.readyState == 4 && this.status == 200) {
+
+        var alergiasArray = JSON.parse(this.responseText);
+        var data = alergiasArray['Alergias'];
+        var alergias = data;
 
 
+        //Matriz para guardar las alergias y su ponderación
+        var datos = [
+            [0],
+            [1],
+            [2],
+            [3],
+            [4],
+            [5],
+            [6],
+            [7],
+            [8],
+            [9]
+        ];
 
-    // var matrizAlergias = [
-    //     [0,[0.4],[0],[0.6],[0],[0.4],[0.7],[0.8]], //hata llegar a 54 (total de sintomas).
-    //     [1,[0.6],[0.2],[0.9],[0.4],[0.7],[0.8]]  //hata llegar a 54 (total de sintomas).
-    //     [2,[]],
-    //     [3,[]],
-    //     [4,[]],
-    //     [5,[]],
-    //     [6,[]],
-    //     [7,[]],
-    //     [8,[]],
-    //     [9,]
-    // ];
+        var acumulador = 0;
+        
+            //llenar la matriz "datos"
+            for (let i = 0; i < Object.keys(alergias).length; i++) {
+                var nameAlergia =  Object.keys(alergias)[i];
+                var arrayParteCuerpo = alergias[nameAlergia]['Sintomas'];
+            
+                for (let y = 0; y < Object.keys(arrayParteCuerpo).length; y++) {
+                    var parteCuerpo = Object.keys(arrayParteCuerpo);
+                    var sintomas = arrayParteCuerpo[parteCuerpo[y]];
+                    var sintoma = Object.keys(arrayParteCuerpo[parteCuerpo[y]]);
+                    
+                    for (let x = 0; x < sintoma.length; x++) {
+                        datos[i][acumulador] = sintomas[sintoma[x]];
+                        acumulador++;
+                    }     
 
+                }
+                acumulador=0;
+            }
 
-    // matrizAlergias[9].push([4]);
+            //llenar la matriz "matrizAlergias" en la posicion correspondiente con pa calificacion correspondiente
+            let y = 1;
+            var posicionCorrecta = 0;
 
-    console.log("this "+matrizAlergias[9]);
+            for (let i = 0; i < matrizAlergias.length; i++) {
+                for (let x = 0; x < datos[i].length; x++) {
+                        posicionCorrecta = datos[i][x].PreguntaId;
+                        do {
+                            if(y = posicionCorrecta){
+                                matrizAlergias[i][y] = datos[i][x].Calificacion;
+                                break;
+                            }else{
+                                y++;
+                            }
+                        } while (y < 53);
+                        y = 1;
+                }
+            }
+            //imprime matriz "matrizAlergias"
+            console.log(matrizAlergias);       
+        }   
+    }
+    request.open('GET', './sources/alergias.json', true);
+    request.send();
 }
