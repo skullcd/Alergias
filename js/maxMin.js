c3434de18f35fb801db87d8689021abaddba34dd
Unknown <carlosd_eg@yahoo.com.mx>
2019-05-29 12:32:03 -0500

    Corregir Maxmin

--- js/maxMin.js
+++ js/maxMin.js
@@ -1,33 +1,35 @@
-var auxSintomas = [
-    [0],
-    [1],
-    [2],
-    [3],
-    [4],
-    [5],
-    [6],
-    [7],
-    [8],
-    [9]
-];
-var auxAlergia = [
-    [0],
-    [1],
-    [2],
-    [3],
-    [4],
-    [5],
-    [6],
-    [7],
-    [8],
-    [9]
-];
-var auxMasAlto = [];
-var auxSumaAlergias = [];
-var suma;
-var aux;
-var sumaAlergias;
+
 function diagnostico(alergias, sintomasUser) {
+  var auxSintomas = [
+      [0],
+      [1],
+      [2],
+      [3],
+      [4],
+      [5],
+      [6],
+      [7],
+      [8],
+      [9]
+  ];
+  var auxAlergia = [
+      [0],
+      [1],
+      [2],
+      [3],
+      [4],
+      [5],
+      [6],
+      [7],
+      [8],
+      [9]
+  ];
+  var auxMasAlto = [];
+  var auxSumaAlergias = [];
+  var suma;
+  var aux;
+  var sumaAlergias;
+  console.log(alergias);
     //Obtener el mínimo de el cruce de síntomas de usuario y síntomas de cada alergia
     for (let i = 0; i < alergias.length; i++) {
         for (let j = 1; j <= alergias[i].length; j++) {
@@ -42,9 +44,9 @@ function diagnostico(alergias, sintomasUser) {
     //Obtener la suma de cada mínimo por alergia/usuario
     for (let i = 0; i < auxSintomas.length; i++) {
         suma = 0;
-        for (let j = 1; j < auxSintomas[i].length-1; j++) {            
+        for (let j = 1; j < auxSintomas[i].length-1; j++) {
             suma += parseFloat(auxSintomas[i][j]);
-        }        
+        }
         auxAlergia[i].push(suma);
     }
 
@@ -52,9 +54,17 @@ function diagnostico(alergias, sintomasUser) {
     for (let i = 0; i < auxAlergia.length; i++) {
         auxMasAlto.push(auxAlergia[i].pop());
     }
-    
+
     //Retorna el valor más alto
-    valorMasAlto = Math.max.apply(Math, auxMasAlto);
+    valorMasAlto = 0;
+    for (var i = 0; i < auxMasAlto.length; i++) {
+      if (valorMasAlto > auxMasAlto[i]) {
+        continue;
+      }else {
+        valorMasAlto = auxMasAlto[i];
+      }
+
+    }
     console.log(valorMasAlto);
 
     //Se obtiene la suma de cada alergia (según sus síntomas)
@@ -65,17 +75,28 @@ function diagnostico(alergias, sintomasUser) {
         }
         auxSumaAlergias.push(sumaAlergias);
     }
-    
+
+    // console.log(auxSumaAlergias);
+    // console.log(alergias);
     //Se compara el valor más alto del usuario con las alergias
-    var kk;
+    var kk=0;
+    for (var i = 0; i < auxSumaAlergias.length; i++) {
+      if (kk > auxSumaAlergias[i]) {
+        continue;
+      }else {
+        kk = auxSumaAlergias[i];
+      }
+
+    }
+    console.log(auxSumaAlergias);
     for (let i = 0; i < auxSumaAlergias.length; i++) {
         if(auxSumaAlergias == valorMasAlto){
             console.log(alergias[i][0]);
         }else{
-            kk = Math.max.apply(Math, auxSumaAlergias);
-            console.log("Hay un " + (parseFloat(parseFloat(valorMasAlto) * 100) / parseFloat(kk)).toFixed(2) + "% de posibilidad de tener " + sintomasUser[0])
+            // kk = Math.max.apply(Math, auxSumaAlergias);
+            console.log("Hay un " + (parseFloat(parseFloat(valorMasAlto) * 100) / parseFloat(auxSumaAlergias[i])).toFixed(2) + "% de posibilidad de tener " + sintomasUser[0])
             break;
         }
     }
-    
+
 }
